9ae02e171711cd03648b599e4015bb07f0c1ca69
codingator <dev.jaehyeo.lee@gmail.com>
2020-11-02 15:18:10 +0900

    modify

--- app/schema/test.sql
+++
@@ -1,8 +0,0 @@
--- CREATE DATABASE testDBdefault CHARACTER SET UTF8;
- 
--- use testDB;
- 
--- CREATE TABLE testTable(
---     idx     INT UNSIGNEDNOT NULL AUTO_INCREMENTPRIMARY KEY,
---     test    VARCHAR(256)NOT NULL
--- ) CHARSET=utf8;
